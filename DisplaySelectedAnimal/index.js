3d5468d91034de6ea6ccf2cf7c9fda9239a6222a
rutupadale131 <padalerutuja@gmail.com>
2025-01-07 15:31:31 +0530

    Files Added

---
+++ DisplaySelectedAnimal/index.js
@@ -0,0 +1,16 @@
+let petsImageUrls = {
+    dog: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-dog-img.png",
+    cat: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-cat-img.png",
+    parrot: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-parrot-img.png",
+    spider: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-spider-img.png",
+    rabbit: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-rabbit-img.png"
+  };
+  
+  const selectBar=document.getElementById("petSelect");
+  const petImage=document.getElementById("petImg");
+  
+  selectBar.addEventListener('change',function(event){
+      let selectedImg=event.target.value;
+      petImage.src=petsImageUrls[selectedImg];
+       petImage.alt = `Image of ${selectedImg}`;
+  });
